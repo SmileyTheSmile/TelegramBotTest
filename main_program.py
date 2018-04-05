603b26f852601565d5b6288033f2ab214d75c1f6
Dan <kdanil01@mail.ru>
2018-04-06 00:09:35 +0300

    2

--- main_program.py
+++ main_program.py
@@ -2,104 +2,203 @@ from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, Conve
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from tools import *
 
-def start(bot, update, user_data):
-    update.message.reply_text("Приветствую! Я бот делающий всякие операции с картами (щедро подаренными мне людьми из Яндекса(ТМ)). В любой момент нашего разговора вы можете написать команду /stop чтобы всё остановить. Пожалуйста выберете язык./nGood day! I'm a bot that does stuff with maps, that were generously donated to me by people from Yandex(TM). At any point in our conversation you can use the command /stop to stop it. Please select your language.", reply_markup=markup)
+def start(bot, update):
+    update.message.reply_text(" Приветствую! Я бот делающий всякие операции с картами (щедро подаренными мне людьми из Яндекса(ТМ)). В любой момент нашего разговора вы можете написать команду /stop чтобы всё остановить. Пожалуйста выберете язык.\n Good day! I'm a bot that does stuff with maps, that were generously donated to me by people from Yandex(TM). At any point in our conversation you can use the command /stop to stop it. Please select your language.", reply_markup=markup1)
     return 1
 
-def stop(bot, update):
-    if user_data['language'] == 'RU':
-        update.message.reply_text('Хорошо. Чтобы начать разговор занаво напишите команду /start.', reply_markup=ReplyKeyboardRemove())
-    elif user_data['language'] == 'EN':
-        update.message.reply_text("Alright. To start a new conversation use the command /start.", reply_markup=ReplyKeyboardRemove())
+def stop(bot, update, user_data):
+    update.message.reply_text('Хорошо. Чтобы начать разговор занаво напишите команду /start.\nAlright. To start a new conversation use the command /start.', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
-def help(bot, update, user_data):
-    if user_data['language'] == 'RU':
-        update.message.reply_text('Я бот, делающий всякую бурду.')
-    elif user_data['language'] == 'EN':
-        update.message.reply_text("I'm a bot that does stuff or whatever.")
-        
+def help(bot, update):
+    update.message.reply_text("Я бот, делающий всякую бурду.\nI'm a bot that does stuff or whatever.")
     
     
-def beginning(bot, update, user_data):
+def beginning(bot, update, user_data, chat_data):
     user_data['language'] = update.message.text
     if user_data['language'] == 'RU':
-        update.message.reply_text('Что вы желаете сделать?\nа)Найти какое - нибудь место по адресу? (Напишите "Найти по адресу" если да)\nб)Найти что находится по определённым координатам? (Напишите "Найти по координатам" если да)')
+        update.message.reply_text('Что вы желаете сделать?\n а)Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=markup2ru)
         return 2
     elif user_data['language'] == 'EN':
-        update.message.reply_text("What do you want me to do?\na)Find something according to it's address? (Reply with 'Find by address' if yes.)\nb)Find something according to it's coordinates? Reply with 'Find by coords' if yes.")
+        update.message.reply_text("What do you want me to do?\n a)Find something according to it's address? (Reply with 'A' if yes.)\n b)Find something according to it's coordinates? Reply with 'B' if yes.", reply_markup=markup2en)
         return 2
     else:
-        update.message.reply_text("I was unable to process whatever you wrote there.Please try again.\nЯ не понял что вы там написали. Пожалуйста попробуйте снова.")
+        update.message.reply_text("I was unable to process whatever you wrote there.Please try again.\n Я не понял что вы там написали. Пожалуйста попробуйте снова.", reply_markup=stop_markup)
         return 1
 
-def first_choice(bot, update, user_data):
+def first_choice(bot, update, user_data, chat_data):
     if user_data['language'] == 'RU':
-        if update.message.text == "Найти по адресу":
-            update.message.reply_text('Напишите адрес объекта.')
+        if update.message.text == "А" or update.message.text =='а':
+            update.message.reply_text('Напишите адрес объекта.', reply_markup=stop_markup)
             return 3
-        elif update.message.text == "Найти по координатам":
-            update.message.reply_text('Напишите координаты в таком формате: "43.34,34.43".')
+        elif update.message.text == "Б" or update.message.text =='б':
+            update.message.reply_text('Напишите координаты в таком формате: "43.34,34.43".', reply_markup=stop_markup)
             return 4
+        else:
+            update.message.reply_text("Такого ответа не было! Попробуйте снова.", reply_markup=stop_markup)
+            return 2          
     elif user_data['language'] == 'EN':
-        if update.message.text == "Find by address":
-            update.message.reply_text("Please give me the object's address.")
+        if update.message.text == "A" or update.message.text =='a':
+            update.message.reply_text("Please give me the object's address.", reply_markup=stop_markup)
             return 3
-        elif update.message.text == "Find by coords":
-            update.message.reply_text("Please give me the object's coordinates in this format: '43.34,34.43'.")
+        elif update.message.text == "B" or update.message.text =='b':
+            update.message.reply_text("Please give me the object's coordinates in this format: '43.34,34.43'.", reply_markup=stop_markup)
             return 4
+        else:
+            update.message.reply_text("There was no option like that! Try again.", reply_markup=stop_markup)
+            return 2 
     
-def address_finder(bot, update, user_data):
+    
+def address_finder(bot, update, user_data, chat_data):
     url = static_mapper(geocoder(update.message.text))
-    if url != 'err1' or 'err2':
+    if url != 'err1' or url !='err2':
         if user_data['language'] == 'RU':
-            text = ''
+            text = ' Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б)Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
+            markup = markup3ru
         elif user_data['language'] == 'EN':
-            text = ''                
+            text = ' What do you want to do now?\n a) Find another point\n b)Find the organisations around the point?\n c) Finish the conversation'    
+            markup = markup3en
         bot.sendPhoto(
             update.message.chat.id,
-            url,
-            text
+            url[0],
+            text,
+            reply_markup=markup
             )
-        return ConversationHandler.END
+        user_data['last_search'] = url[1]
+        return 5
     elif url == 'err1':
         if lan == 'RU':
-            update.message.reply_text("По вашему запросу ничего не было найдено. Пожалуйста попробуйте снова.")
+            update.message.reply_text("По вашему запросу ничего не было найдено. Пожалуйста попробуйте снова.", reply_markup=stop_markup)
         elif lan == 'EN':
-            update.message.reply_text("Nothing was found using the data you've given. Please try again")
+            update.message.reply_text("Nothing was found using the data you've given. Please try again", reply_markup=stop_markup)
         return 3
     elif url == 'err2':
         if lan == 'RU':
-            update.message.reply_text("Во время выполнения вашего запроса возникла ошибка. Пожалуйста попробуйте снова.")
+            update.message.reply_text("Во время выполнения вашего запроса возникла ошибка. Пожалуйста попробуйте снова.", reply_markup=stop_markup)
         elif lan == 'EN':
-            update.message.reply_text("There was an error processing your request. Please try again.")
+            update.message.reply_text("There was an error processing your request. Please try again.", reply_markup=stop_markup)
         return 3
 
-def coords_finder(bot, update, user_data):
-    url = static_mapper(update.message.text)
-    request = requests.get(url)
+def coords_finder(bot, update, user_data, chat_data):
+    url = static_mapper((update.message.text, ('0.02', '0.02')))
+    request = requests.get(url[0])
     if request.status_code == 200:
         if user_data['language'] == 'RU':
-            text = ''
+            text = ' Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б)Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
+            markup = markup3ru
         elif user_data['language'] == 'EN':
-            text = ''                
+            text = ' What do you want to do now?\n a) Find another point\n b)Find the organisations around the point?\n c) Finish the conversation'      
+            markup = markup3en
         bot.sendPhoto(
             update.message.chat.id,
-            address,
-            text
+            url[0],
+            text,
+            reply_markup=markup
             )
-        return ConversationHandler.END
+        user_data['last_search'] = url[1]
+        return 5
     else:
-        if lan == 'RU':
-            update.message.reply_text("Во время выполнения вашего запроса возникла ошибка. Пожалуйста попробуйте снова.")
-        elif lan == 'EN':
-            update.message.reply_text("There was an error processing your request. Please try again.")
+        if user_data['language'] == 'RU':
+            update.message.reply_text("Во время выполнения вашего запроса возникла ошибка. Пожалуйста попробуйте снова.", reply_markup=stop_markup)
+        elif user_data['language'] == 'EN':
+            update.message.reply_text("There was an error processing your request. Please try again.", reply_markup=stop_markup)
         return 4
+    
+def second_choice(bot, update, user_data, chat_data):
+    if user_data['language'] == 'RU':
+        if update.message.text == "А" or update.message.text == 'а':
+            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=choice_keyboard2ru)
+            return 2
+        elif update.message.text == "Б" or update.message.text == 'б':
+            update.message.reply_text('В каком радиусе от найденной точки нужно найти организации?', reply_markup=stop_markup)
+            return 6
+        elif update.message.text == "В" or update.message.text == 'в':
+            update.message.reply_text('Хорошо. Чтобы начать разговор занаво напишите команду /start.', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        else:
+            update.message.reply_text("Такого ответа не было! Попробуйте снова.")
+            return 5         
+    elif user_data['language'] == 'EN':
+        if update.message.text == "A" or update.message.text == 'a':
+            update.message.reply_text('What do you want to do?\n a)Find a place according to its address? (Reply with "A" if yes)\n b)Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=choice_keyboard2en)
+            return 2
+        elif update.message.text == "B" or update.message.text == 'b':
+            update.message.reply_text('At what radius from the point would you like to find the organisations?', reply_markup=stop_markup)
+            return 6
+        elif update.message.text == "C" or update.message.text == 'c':
+            update.message.reply_text("Alright. To start a new conversation use the command /start.", reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        else:
+            update.message.reply_text("That ain't an answer! Try again.")
+            return 5      
 
+def organisations_finder(bot, update, user_data, chat_data):
+    spn = update.message.text
+    organisations = get_organisation(user_data['last_search'], spn)
+    if organisations != 'err5':
+        url = static_mapper((user_data['last_search'], ('0.02', '0.02')), organisations)
+        request = requests.get(url[0])
+        if request.status_code == 200:
+            if user_data['language'] == 'RU':
+                text = ' Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б) Завершить обслуживание'
+                markup = markup2ru
+            elif user_data['language'] == 'EN':
+                text = ' What do you want to do now?\n a) Find another point\n b) Finish the conversation'   
+                markup = markup2en
+            bot.sendPhoto(
+                update.message.chat.id,
+                url[0],
+                text,
+                reply_markup = markup
+                )
+            user_data['last_search'] = url[1]
+            return 7
+        else:
+            if user_data['language'] == 'RU':
+                update.message.reply_text("Во время выполнения вашего запроса возникла ошибка. Пожалуйста попробуйте снова.")
+            elif user_data['language'] == 'EN':
+                update.message.reply_text("There was an error processing your request. Please try again.")
+    else:
+        if user_data['language'] == 'RU':
+            update.message.reply_text("Ничего не было найдено. Попробуйте поискать поближе к цивилизации.")
+        elif user_data['language'] == 'EN':
+            update.message.reply_text("Nothing was found. Try searching somewhere closer to civilization.")
+    return 6
+    
+def third_choice(bot, update, user_data, chat_data):
+    if user_data['language'] == 'RU':
+        if update.message.text == "А" or update.message.text == 'а':
+            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=choice_keyboard2ru)
+            return 2
+        elif update.message.text == "Б" or update.message.text == 'б':
+            update.message.reply_text('Хорошо. Чтобы начать разговор занаво напишите команду /start.', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        else:
+            update.message.reply_text("Такого ответа не было! Попробуйте снова.")
+            return 5         
+    elif user_data['language'] == 'EN':
+        if update.message.text == "A" or update.message.text == 'a':
+            update.message.reply_text('What do you want to do?\n a)Find a place according to its address? (Reply with "A" if yes)\n b)Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=choice_keyboard2en)
+            return 2
+        elif update.message.text == "B" or update.message.text == 'b':
+            update.message.reply_text("Alright. To start a new conversation use the command /start.", reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        else:
+            update.message.reply_text("That ain't an answer! Try again.")
+            return 5      
 
-reply_keyboard = [['/start', '/stop'],
-                  ['/help']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+stop_keyboard = [['/start', '/stop']]
+choice_keyboard1 = [['RU', 'EN'], ['/stop']]
+choice_keyboard2ru = [['А', 'Б'], ['/stop']]
+choice_keyboard2en = [['A', 'B'], ['/stop']]
+choice_keyboard3ru = [['А', 'Б', 'В'], ['/stop']]
+choice_keyboard3en = [['A', 'B', 'C'], ['/stop']]
+stop_markup = ReplyKeyboardMarkup(stop_keyboard, one_time_keyboard=False)
+markup1 = ReplyKeyboardMarkup(choice_keyboard1, one_time_keyboard=False)
+markup2ru = ReplyKeyboardMarkup(choice_keyboard2ru, one_time_keyboard=False)
+markup2en = ReplyKeyboardMarkup(choice_keyboard2en, one_time_keyboard=False)
+markup3ru = ReplyKeyboardMarkup(choice_keyboard3ru, one_time_keyboard=False)
+markup3en = ReplyKeyboardMarkup(choice_keyboard3en, one_time_keyboard=False)
 
 
 def main():
@@ -111,12 +210,15 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            1: [MessageHandler(Filters.text, beginning)],
-            2: [MessageHandler(Filters.text, first_choice)],
-            3: [MessageHandler(Filters.text, address_finder)],
-            4: [MessageHandler(Filters.text, coords_finder)],            
+            1: [MessageHandler(Filters.text, beginning, pass_chat_data=True, pass_user_data=True)],
+            2: [MessageHandler(Filters.text, first_choice, pass_chat_data=True, pass_user_data=True)],
+            3: [MessageHandler(Filters.text, address_finder, pass_chat_data=True, pass_user_data=True)],
+            4: [MessageHandler(Filters.text, coords_finder, pass_chat_data=True, pass_user_data=True)],  
+            5: [MessageHandler(Filters.text, second_choice, pass_chat_data=True, pass_user_data=True)],
+            6: [MessageHandler(Filters.text, organisations_finder, pass_chat_data=True, pass_user_data=True)],
+            7: [MessageHandler(Filters.text, third_choice, pass_chat_data=True, pass_user_data=True)]
         },
-        fallbacks=[CommandHandler('stop', stop)]
+        fallbacks=[CommandHandler('stop', stop, pass_user_data=True)]
     )
     
     dp.add_handler(conv_handler) 
--- tools.py
+++ tools.py
@@ -1,12 +1,13 @@
 import requests
+import math
 
-def get_ll(obj):
+def get_ll_spn(obj):
     coords = obj["Point"]["pos"]
     lowerx, lowery = obj["boundedBy"]["Envelope"]["lowerCorner"].split()
     upperx, uppery = obj["boundedBy"]["Envelope"]["upperCorner"].split()
     deltax = (float(upperx) - float(lowerx)) / 2
     deltay = (float(uppery) - float(lowery)) / 2
-    return ','.join(coords.split())
+    return (','.join(coords.split()), (deltax,deltay))
 
 def geocoder(text):
     try:
@@ -18,19 +19,59 @@ def geocoder(text):
         toponym = response.json()
         if len(toponym["response"]["GeoObjectCollection"]["featureMember"]) != 0:
             toponym = toponym["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-            return get_ll(toponym)
+            return get_ll_spn(toponym)
         else:
             return 'err1'    
     except Exception:
         return 'err2'   
 
-def static_mapper(data):
-    ll = data
-    if ll != 'err1' or 'err2':
-        spn = "0.02"
+def static_mapper(data, points = []):
+    if data != 'err1' or 'err2':
+        ll = data[0]
+        spn = data[1]
+        if points != []:
+            points = '~' + '~'.join(points)
+        else: points = ''
         static_api_address = "http://static-maps.yandex.ru/1.x/?"
-        static_api_params = "ll={}&spn={}&pt={}&l=map".format(ll, spn, ll)
+        static_api_params = "ll={}&spn={},{}&pt={}&l=map".format(ll, spn[0], spn[1], ll + points)
         address = static_api_address + static_api_params
-        return address
-    else:
-        return ll
+        print(address)
+        return (address, ll)
+    return ll
+
+def lonlat_distance(a, b):
+    degree_to_meters_factor = 111 * 1000
+    a_lon, a_lat = a[0], a[1]
+    b_lon, b_lat = b[0], b[1]
+
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    distance = math.sqrt(dx * dx + dy * dy)
+    return distance
+
+def get_organisation(coords, spn):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "3c4a592e-c4c0-4949-85d1-97291c87825c"
+    
+    search_params = {
+        "apikey": api_key,
+        "lang": "ru_RU",
+        "ll": coords,
+        "type": "biz"
+    }
+    
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        return "err4"
+    json_response = response.json()
+    if len(json_response["features"]) != 0:
+        points = []
+        for i in json_response["features"]:
+            if lonlat_distance(i["geometry"]["coordinates"], list(map(float, coords.split(',')))) <= int(spn):
+                points.append(','.join(map(str, i["geometry"]["coordinates"])))
+        return points
+    return "err5"
