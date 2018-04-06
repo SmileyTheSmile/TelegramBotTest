d405e6a092d6f149c47001beba402c3d78a44e1d
Dan <kdanil01@mail.ru>
2018-04-06 15:58:26 +0300

    4

--- main_program.py
+++ main_program.py
@@ -3,7 +3,7 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from tools import *
 
 def start(bot, update):
-    update.message.reply_text(" Приветствую! Я бот делающий всякие операции с картами (щедро подаренными мне людьми из Яндекса(ТМ)). В любой момент нашего разговора вы можете написать команду /stop чтобы всё остановить. Пожалуйста выберете язык.\n Good day! I'm a bot that does stuff with maps, that were generously donated to me by people from Yandex(TM). At any point in our conversation you can use the command /stop to stop it. Please select your language.", reply_markup=markup1)
+    update.message.reply_text("Приветствую! Я бот делающий всякие операции с картами (щедро подаренными мне людьми из Яндекса(ТМ)). В любой момент нашего разговора вы можете написать команду /stop чтобы всё остановить. Пожалуйста выберете язык.\nGood day! I'm a bot that does stuff with maps, that were generously donated to me by people from Yandex(TM). At any point in our conversation you can use the command /stop to stop it. Please select your language.\nRU/EN", reply_markup=markup1)
     return 1
 
 def stop(bot, update, user_data):
@@ -17,10 +17,10 @@ def help(bot, update):
 def beginning(bot, update, user_data, chat_data):
     user_data['language'] = update.message.text
     if user_data['language'] == 'RU':
-        update.message.reply_text('Что вы желаете сделать?\n а)Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=markup2ru)
+        update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б) Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=markup2ru)
         return 2
     elif user_data['language'] == 'EN':
-        update.message.reply_text("What do you want me to do?\n a)Find something according to it's address? (Reply with 'A' if yes.)\n b)Find something according to it's coordinates? Reply with 'B' if yes.", reply_markup=markup2en)
+        update.message.reply_text("What do you want me to do?\n a) Find something according to it's address? (Reply with 'A' if yes.)\n b) Find something according to it's coordinates? Reply with 'B' if yes.", reply_markup=markup2en)
         return 2
     else:
         update.message.reply_text("I was unable to process whatever you wrote there.Please try again.\n Я не понял что вы там написали. Пожалуйста попробуйте снова.", reply_markup=markup2ru)
@@ -53,10 +53,10 @@ def address_finder(bot, update, user_data, chat_data):
     url = static_mapper(geocoder(update.message.text))
     if url != 'err1' or url !='err2':
         if user_data['language'] == 'RU':
-            text = ' Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б)Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
+            text = 'Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б) Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
             markup = markup3ru
         elif user_data['language'] == 'EN':
-            text = ' What do you want to do now?\n a) Find another point\n b)Find the organisations around the point?\n c) Finish the conversation'    
+            text = ' What do you want to do now?\n a) Find another point\n b) Find the organisations around the point?\n c) Finish the conversation'    
             markup = markup3en
         bot.sendPhoto(
             update.message.chat.id,
@@ -84,10 +84,10 @@ def coords_finder(bot, update, user_data, chat_data):
     request = requests.get(url[0])
     if request.status_code == 200:
         if user_data['language'] == 'RU':
-            text = ' Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б)Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
+            text = 'Что вы хотите сделать теперь?\n а) Найти ещё одну точку\n б) Найти предприятия вокруг этой точки?\n в) Завершить обслуживание'
             markup = markup3ru
         elif user_data['language'] == 'EN':
-            text = ' What do you want to do now?\n a) Find another point\n b)Find the organisations around the point?\n c) Finish the conversation'      
+            text = 'What do you want to do now?\n a) Find another point\n b) Find the organisations around the point?\n c) Finish the conversation'      
             markup = markup3en
         bot.sendPhoto(
             update.message.chat.id,
@@ -107,7 +107,7 @@ def coords_finder(bot, update, user_data, chat_data):
 def second_choice(bot, update, user_data, chat_data):
     if user_data['language'] == 'RU':
         if update.message.text == "А" or update.message.text == 'а':
-            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=choice_keyboard2ru)
+            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б) Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=choice_keyboard2ru)
             return 2
         elif update.message.text == "Б" or update.message.text == 'б':
             update.message.reply_text('В каком радиусе от найденной точки нужно найти организации?', reply_markup=stop_markup)
@@ -120,7 +120,7 @@ def second_choice(bot, update, user_data, chat_data):
             return 5         
     elif user_data['language'] == 'EN':
         if update.message.text == "A" or update.message.text == 'a':
-            update.message.reply_text('What do you want to do?\n a)Find a place according to its address? (Reply with "A" if yes)\n b)Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=choice_keyboard2en)
+            update.message.reply_text('What do you want to do?\n a) Find a place according to its address? (Reply with "A" if yes)\n b) Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=choice_keyboard2en)
             return 2
         elif update.message.text == "B" or update.message.text == 'b':
             update.message.reply_text('At what radius from the point would you like to find the organisations?', reply_markup=stop_markup)
@@ -169,7 +169,7 @@ def organisations_finder(bot, update, user_data, chat_data):
 def third_choice(bot, update, user_data, chat_data):
     if user_data['language'] == 'RU':
         if update.message.text == "А" or update.message.text == 'а':
-            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б)Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=markup2ru)
+            update.message.reply_text('Что вы желаете сделать?\n а) Найти какое - нибудь место по адресу? (Напишите "А" если да)\n б) Найти что находится по определённым координатам? (Напишите "Б" если да)', reply_markup=markup2ru)
             return 2
         elif update.message.text == "Б" or update.message.text == 'б':
             update.message.reply_text('Хорошо. Чтобы начать разговор занаво напишите команду /start.', reply_markup=ReplyKeyboardRemove())
@@ -179,7 +179,7 @@ def third_choice(bot, update, user_data, chat_data):
             return 5         
     elif user_data['language'] == 'EN':
         if update.message.text == "A" or update.message.text == 'a':
-            update.message.reply_text('What do you want to do?\n a)Find a place according to its address? (Reply with "A" if yes)\n b)Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=markup2en)
+            update.message.reply_text('What do you want to do?\n a) Find a place according to its address? (Reply with "A" if yes)\n b) Find something according to its coordinates? (Reply with "B" if yes)', reply_markup=markup2en)
             return 2
         elif update.message.text == "B" or update.message.text == 'b':
             update.message.reply_text("Alright. To start a new conversation use the command /start.", reply_markup=ReplyKeyboardRemove())
